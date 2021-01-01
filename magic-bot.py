f6f51133cff32f65ab5b0dd98107a5b48214b3c0
Nikolai <nvanbaak@gmail.com>
2021-01-01 12:34:27 -0800

    bot says hello

--- magic-bot.py
+++ magic-bot.py
@@ -5,14 +5,16 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('Bot successfully logged in as: {0.user}').format(client)
+    print('Bot successfully logged in as: {0.user}'.format(client))
+
 
 @client.event
-async def on_message(message)
-    if message.author == client.user
+async def on_message(message):
+    print(message)
+    if message.author == client.user:
         return
     
     if message.content.startswith('$hello'):
-        await message.channel.send('Hello {message.author}!')
+        await message.channel.send('Hello {message.author.name}!'.format(message=message))
 
 client.run(config.bot_token)
