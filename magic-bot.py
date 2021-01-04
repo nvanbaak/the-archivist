144ac7c8d5d9d16c94bc7c54e39a4d4697143c3d
Nikolai <nvanbaak@gmail.com>
2021-01-04 00:47:23 -0800

    refactored to state manager but somehow broke events?

--- magic-bot.py
+++ magic-bot.py
@@ -4,8 +4,6 @@ import random
 from threading import Timer
 
 client = discord.Client()
-game_channel = client.get_channel(config.game_channel_id)
-current_game = None
 
 class Game:
     def __init__(self):
@@ -893,10 +891,32 @@ class Reminder:
         reponse_str = " ".join(self.args)
         print(response_str)
 
-class Sanitizer:
+class Data_Manager:
     def __init__(self):
         self.games = []
         self.output = "gamehistory_test.txt"
+
+    # this is our transit center function
+    def handle_command(self, message_obj):
+        # First, weed out unauthorized users
+        if message_obj.author.id != config.admin_id:
+            return "My apologies, sir. You are not authorized to access the data editor."
+
+        # Next, split string into command words
+        args = message_obj.content.split(" ")
+        
+        if len(args) == 1:
+            return ""
+
+        # Then route
+        if args[1] == "load" or args[1] == "reload":
+            return self.load_games()
+        elif self.games:
+            pass
+        else:
+            return "The data manager currently has nothing loaded.  To get started, type: ```$data load```"
+
+
 
     # loads all games from memory
     def load_games(self):
@@ -917,7 +937,7 @@ class Sanitizer:
 
             self.output_history("backup.txt")
 
-        return "Data editor loaded {num} games from memory and backed up to backup.txt".format(num=len(self.games))
+        return "Data manager loaded {num} games from memory and backed up to backup.txt".format(num=len(self.games))
 
     # outputs local game history to a text file
     def output_history(self, destination):
@@ -926,98 +946,121 @@ class Sanitizer:
         
         return "Wrote game history to {dest}".format(dest=destination)
 
+class State_Manager:
+    def __init__(self):
+        self.game_channel = None
+        self.current_game = None
+    
+    def set_channel(channel_obj):
+        this.game_channel = channel_obj
+        return "Set channel to {channel}".format(channel=channel_obj)
+
+    def route_message(message, stats, dm):
+        if message.author == client.user:
+            return
+    
+        print(message)
+
+        if message.content.startswith('$hello'):
+            await message.channel.send('Hello {message.author.name}!'.format(message=message))
+
+        if message.content.startswith('$randomEDH'):
+            await message.channel.send(stats.random_game().game_state())
+
+        if message.content.startswith('$notif') or message.content.startswith('$remind'):
+            remind = Reminder(message)
+            remind.remind()
+
+        if message.content.startswith('$stats'):
+            response = stats.handle_command(message)
+
+            if response == "":
+                pass
+            else:
+                await message.channel.send(response)
+
+        if message.content.startswith('$game'):
+            # Check if there is is a game
+
+            # user might just be checking if there's a game; we don't need to start a new one in that case
+            status_update = message.content.startswith('$game status') or message.content.startswith('$game state')
+
+            if status_update and self.current_game is None:
+                await self.game_channel.send("There is currently no active game.")
+                return
+
+            # likewise, if they're just using the help menu, we don't need a new game either
+            elif message.content.startswith('$game help'):
+
+                help_str = "The $game menu is used to track __ongoing games of EDH__."
+                help_str += "\n Begin by adding your name and deck with the following command:"
+                help_str += "\n```$game player [your name] [commander name]```"
+                help_str += "(note that there can't be any spaces in your commander's name or the bot will just take the first word)"
+                help_str += "\n Once all players are added, start the game with this command:"
+                help_str += "\n```$game first [name of the player who's going first]```"
+                help_str += "Once in game, keep track of eliminations using the ```$game elim [player]``` command.  Posterity will thank you when you can point out that you're much more likely to die first and therefore not a threat!"
+                help_str += "\n Declare victory with ```$game winner [player]``` Afterward, you can add commentary to the game record so everyone knows how awesome you are, or alternately how little your opponent deserved the win."
+                help_str += "\n Once you're done, type ```$game end``` to store the game in memory. That's it!  You're done!"
+
+                await message.channel.send(help_str)
+                return
+
+            elif current_game is None:
+                # Make a new game
+                self.current_game = Game()
+                await self.game_channel.send("Started a new game!")
+
+            # There's an active game either way at this point, so we have it handle the message
+            response = self.current_game.handle_command(message)
+
+            if response == "end":
+                # store game data
+                self.current_game.store_data("gamehistory.txt")
+
+                #refresh stats engine
+                stats.refresh()
+                
+                # close out the game
+                self.current_game = None
+
+                # confirmation message
+                await message.channel.send("Thanks for playing!")
+
+            # this is the "quit without saving" option
+            elif response == "cancel":
+                self.current_game = None
+                await self.game_channel.send("I have cancelled the game for you.")
+
+            elif response == "":
+                pass
+
+            else:
+                # send the reponse as a message
+                await self.game_channel.send(response)
+
+        if message.content.startswith('$data'):
+            # global game_channel
+            response = dm.handle_command(message)
+            await self.game_channel.send(response)
 
 
 
+
+
+
+
+# create instances of stats engine and data manager
 stats = Statistics()
+dm = Data_Manager()
+state_manager = State_Manager()
 
 @client.event
 async def on_ready():
     print('Bot successfully logged in as: {user}'.format(user=client.user))
+    state_manager.set_channel(client.get_channel(config.game_channel_id)
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    print(message)
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello {message.author.name}!'.format(message=message))
-
-    if message.content.startswith('$randomEDH'):
-        await message.channel.send(stats.random_game().game_state())
-
-    if message.content.startswith('$notif') or message.content.startswith('$remind'):
-        remind = Reminder(message)
-        remind.remind()
-
-    if message.content.startswith('$stats'):
-        response = stats.handle_command(message)
-
-        if response == "":
-            pass
-        else:
-            await message.channel.send(response)
-
-    if message.content.startswith('$game'):
-        # Check if there is is a game
-        global current_game
-
-        # user might just be checking if there's a game; we don't need to start a new one in that case
-        status_update = message.content.startswith('$game status') or message.content.startswith('$game state')
-
-        if status_update and current_game is None:
-            await game_channel.send("There is currently no active game.")
-            return
-
-        # likewise, if they're just using the help menu, we don't need a new game either
-        elif message.content.startswith('$game help'):
-
-            help_str = "The $game menu is used to track __ongoing games of EDH__."
-            help_str += "\n Begin by adding your name and deck with the following command:"
-            help_str += "\n```$game player [your name] [commander name]```"
-            help_str += "(note that there can't be any spaces in your commander's name or the bot will just take the first word)"
-            help_str += "\n Once all players are added, start the game with this command:"
-            help_str += "\n```$game first [name of the player who's going first]```"
-            help_str += "Once in game, keep track of eliminations using the ```$game elim [player]``` command.  Posterity will thank you when you can point out that you're much more likely to die first and therefore not a threat!"
-            help_str += "\n Declare victory with ```$game winner [player]``` Afterward, you can add commentary to the game record so everyone knows how awesome you are, or alternately how little your opponent deserved the win."
-            help_str += "\n Once you're done, type ```$game end``` to store the game in memory. That's it!  You're done!"
-
-            await message.channel.send(help_str)
-            return
-
-        elif current_game is None:
-            # Make a new game
-            current_game = Game()
-            await game_channel.send("Started a new game!")
-
-        # There's an active game either way at this point, so we have it handle the message
-        response = current_game.handle_command(message)
-
-        if response == "end":
-            # store game data
-            current_game.store_data("gamehistory.txt")
-
-            #refresh stats engine
-            stats.refresh()
-            
-            # close out the game
-            current_game = None
-
-            # confirmation message
-            await message.channel.send("Thanks for playing!")
-
-        # this is the "quit without saving" option
-        elif response == "cancel":
-            current_game = None
-            await game_channel.send("I have cancelled the game for you.")
-
-        elif response == "":
-            pass
-
-        else:
-            # send the reponse as a message
-            await game_channel.send(response)
+    state_manager.route_message(message, stats, dm)
 
 client.run(config.bot_token)
