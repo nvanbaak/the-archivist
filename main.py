6be72069f934071391ef94b5af5bc231df9aa1bf
Nikolai <nvanbaak@gmail.com>
2021-04-06 12:34:06 -0700

    propery lists players when there are multiple players

--- main.py
+++ main.py
@@ -59,6 +59,18 @@ class Lobby:
         except ValueError:
             return "Error — attempted to remove non-existant player {player} from lobby **{lobby}**!".format(player=player, lobby=self.name)
 
+    def list_players(self):
+        if self.players:
+            response = "["
+            list_str = ""
+            for player in self.players:
+                list_str += ", {player}".format(player=player.name)
+            response += list_str[2:]
+            response += "]"
+            return response
+        else:
+            return ""
+
 class State_Manager:
     def __init__(self):
         # Channel setup
@@ -219,14 +231,7 @@ class State_Manager:
                 lobby_list_str += ", {lobby}".format(lobby=lobby)
 
                 # if there are players, list them
-                if lobby.players:
-                    player_list_str = ""
-                    for player in lobby.players:
-                        player_list_str += ", {player}".format(player=player[:-5])
-
-                    # add brackets and delete the leading comma
-                    player_list_str = "[" + player_list_str[2:]
-                    player_list_str += "]"
+                lobby_list_str += self.active_lobbies[lobby].list_players() 
 
             response += lobby_list_str[2:]
 
@@ -282,9 +287,14 @@ class State_Manager:
         # $Game commands
 
 
+        if message.content.startswith("$set output"):
+            response = self.set_channel(message.channel)
 
         # Send confirmation message to Discord
-        await self.game_channel.send(response)
+        if response != "":
+            await self.game_channel.send(response)
+        else:
+            return
 
         ######
         # These functions use the pre-lobby architecture
@@ -299,9 +309,6 @@ class State_Manager:
 
             else:
                 await message.channel.send('Hello {message.author.name}!'.format(message=message))
-
-        if message.content.startswith("$set output"):
-            return self.set_channel(message.channel)
 
         if message.content.startswith("$lorem"):
             lorem = "Token engine firebreathing blinking mono blue Rakdos Steve stabilize Grixis interrupt restricted mono white Rakdos 4 turn clock decking fixing sideboard Selesnya dome Chimney Pimp big butt decking gas bolt 3 drop chump tank token enchantment topdeck mana pool tank dork utility land standard race card swing race nut draw artifact Golgari dig beatstick race hate mono green Sultai pro mana rock Naya pro-blue Vorthos pop off dig for an answer Orzhov CMC wheel Canadian Threshold land for turn Abzan tank maindeck grindy trade blow up Temur nut draw Sultai chump meta top 8 meta combat trick fatty sorcery Blinky power 9 hate mono black pro-black 2 drop drain fixing in the air modern beatstick prison Vorthos stabilize artifact mirror match Chimney Pimp moxen hard cast hydra land drop grinder aggro aggro Simic Esper weenie 4 turn clock 4 turn clock hate exile enchantment Naya Naya big butt cantrip fetch dig reanimate restricted proxy go off legacy grinder protection interrupt maindeck decking Naya Grixis prison creatureball 2 drop hate bear beats wedge 2 drop wheel enchantment vintage playset Temur 4 turn clock mana birds decking vanilla pro-blue EDH shock land wedge crack fetch wheel durdler mill Blinky chump block mana pool curve card pool mana hose dragon reanimate moxes glass cannon Gruul mana dork sorcery clock hate bear fetch token engine mana hose tempo pro-black card Timmy Grixis\n\nCheck land playset threat Izzet shock on a stick allied colors blow up bomb grind in response Grixis value legacy fixing race legacy glass cannon land for turn combo shock land durdler Grixis mulligan reanimate deck hoser Steve proxy wedge Gruul reanimate jankey mana hose aggro response pro-white pro-red combat trick CMC board state token ping Academy bolt mill he's got a bit of an ass on him hate mana pool Jund board presence blow up Canadian Threshold dig color Timmy pro-red prime time mana pool ping reanimator bomb reanimator mulligan dragon gas dork shock keeper man land wheel shock on a stick aggro big butt playset on a stick beefcake synergy reanimate allied colors control Jeskai fixing jankey 1 drop bear Esper 1 drop board presence bomb shell mana birds go wide Boros grindy hard cast pain land vintage cut interrupt sorcery blink Selesnya finisher metagame control reanimate mana screwed beatstick blow up ping board presence Mardu ritual out of gas Gruul card pool bolt archetype stabilize mono black beefcake pro-white chump block dead card bomb pain land blow up pump jankey combo pain land tutor 2 drop beatdown Mardu planeswalker dragon swing hydra fixing Izzet allied colors mana pool nut draw prison control hate bear modern run Jund land for turn legendary cut fixing ETB lethal shock chump voltron Gruul mana dork chump block pro-black moxen restricted playset Bob card pool"
